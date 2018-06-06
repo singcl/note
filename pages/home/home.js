6f1a5429cfff3bb5f0ed3278c3c25022adcd7254
singcl <24661881@qq.com>
2018-06-06 16:47:30 +0800

    :art: 简单笔记编辑页wxml & wxss 构建中...

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,14 @@
 Page({
-    //
+    data: {
+        list: [],
+        index: 0,
+        date: '2018-06-06',
+        checkStatus: false,
+        checkItem: false,
+        url: '../../images/check_false.png'
+    },
+
+    onLoad: function() {
+        //
+    }
 });
